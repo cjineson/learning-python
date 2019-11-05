ae674298b3d9cfd1802d51495d85fce3b6fbc225
Chris Ineson <chrisineson@Chriss-MBP.lan>
2019-11-05 22:24:58 +0000

    Tweak formatting

--- 5_passwordgen.py
+++ 5_passwordgen.py
@@ -5,14 +5,14 @@ length = 0
 
 if len(sys.argv) > 1:
     length = int(sys.argv[1])
-    print('Password length:',length)
+    print("Password length:",length)
 else:
     length = int(input("Password length:"))
-    print('Password length:',length)
+    print("Password length:",length)
 
 i = 1
 while i<length:
     sys.stdout.write(random.choice(string.ascii_letters))
     sys.stdout.flush()
     i=i+1
-print
+print()
