beb4c58aff07f4ddd25019fc812ac7ba19491321
332539 <a332539@332539s-MBP.lan>
2019-11-11 19:52:19 +0000

    Add importer/importee

---
+++ importee.py
@@ -0,0 +1,5 @@
+def sayhello():
+    print('Hello World!')
+
+if __name__ == '__main__':
+    print('Hello World you ran this as the main module')
---
+++ importer.py
@@ -0,0 +1,3 @@
+import importee
+
+importee.sayhello()
